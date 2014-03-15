76014f503191e6a66094ff241949d80a89414741
Giovanni Capuano <webmaster@giovannicapuano.net>
2014-03-15 12:06:39 +0100

    Fix indentation

--- public/javascripts/improvisation/main.js
+++ public/javascripts/improvisation/main.js
@@ -6,41 +6,42 @@
  
 define(function(require) {
   var Backbone            = require('Backbone'             );
-	var ImprovisationModel  = require('./ImprovisationModel' );
-	var ImprovisationView   = require('./ImprovisationView'  );
-	var ImprovisationRouter = require('./ImprovisationRouter');
-	var $                   = require('jquery'               );
+  var ImprovisationModel  = require('./ImprovisationModel' );
+  var ImprovisationView   = require('./ImprovisationView'  );
+  var ImprovisationRouter = require('./ImprovisationRouter');
+  var $                   = require('jquery'               );
 
-	var router = new ImprovisationRouter;
-	var prevId = '';
+  var router = new ImprovisationRouter;
+  var prevId = '';
 
-	router.on('route:getWall', function(id) {
-		if(id == '../') {
-			var params = prevId.split('/');
-			    id     = params[params.length - 2];
+  router.on('route:getWall', function(id) {
+    console.log(id);
+    if(id == '../') {
+      var params = prevId.split('/');
+          id     = params[params.length - 2];
 
-			if(id === undefined)
-				id = '';
-				
-			new Backbone.Router().navigate('#/wall/' + id, { trigger: true });
-		}
-		else if(id == null)
-			id = '';
+      if(id === undefined)
+        id = '';
+        
+      new Backbone.Router().navigate('#/wall/' + id, { trigger: true });
+    }
+    else if(id == null)
+      id = '';
 
-		var model = new ImprovisationModel({ id: id });
-		model.fetch();
+    var model = new ImprovisationModel({ id: id });
+    model.fetch();
 
-		var improvisation = new ImprovisationView({
-			el   : $('.improvisation').first(),
-			model: model
-		});
+    var improvisation = new ImprovisationView({
+      el   : $('.improvisation').first(),
+      model: model
+    });
 
-		prevId = id;
-	});
+    prevId = id;
+  });
 
-	router.on('route:default', function(actions) {
-		new Backbone.Router().navigate('#/wall/', { trigger: true });
-	});
+  router.on('route:default', function(actions) {
+    new Backbone.Router().navigate('#/wall/', { trigger: true });
+  });
 
   Backbone.history.start();
 });
