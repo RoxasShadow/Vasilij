971eb2b508d2a95788c8d70e55714bc41ae00203
Giovanni Capuano <webmaster@giovannicapuano.net>
2014-03-27 19:09:57 +0100

    Fix run.js indentation

--- public/javascripts/run.js
+++ public/javascripts/run.js
@@ -7,15 +7,15 @@
 var curl;
 
 (function() {
-	curl({
-		main    : 'improvisation',
-		packages: {
-			improvisation: { location: '/javascripts/improvisation'                        },
-			curl         : { location: '/javascripts/lib/curl/src/curl'                    },
-			jquery       : { location: '/javascripts/lib/jquery/jquery',         main: '.' },
-			Backbone     : { location: '/javascripts/lib/backbone-amd/backbone', main: '.' },
-			underscore   : { location: '/javascripts/lib/lodash/lodash',         main: '.' },
+  curl({
+    main    : 'improvisation',
+    packages: {
+      improvisation: { location: '/javascripts/improvisation'                        },
+      curl         : { location: '/javascripts/lib/curl/src/curl'                    },
+      jquery       : { location: '/javascripts/lib/jquery/jquery',         main: '.' },
+      Backbone     : { location: '/javascripts/lib/backbone-amd/backbone', main: '.' },
+      underscore   : { location: '/javascripts/lib/lodash/lodash',         main: '.' },
       humanize     : { location: '/javascripts/lib/humanize/humanize',     main: '.' }
-		}
-	});
+    }
+  });
 }());
